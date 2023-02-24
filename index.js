26a39489908f4f0ed8c80dd822214fdebbc78104
Tomás Belmar da Costa <tomasbelmarcosta@gmail.com>
2023-02-24 16:01:10 +0100

    developed Present class

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ Last edited 2023
 Designed for use with socket.io, but configurable for any 
 */
 
-class BirthdayPresent {
+module.exports = {
+  DeliveryService,
+  Present,
+};
+
+class DeliveryService {
   static ROOM_SEND = "BDAY_PRESENT_ROOM_SEND";
   static ROOM_RCV = "BDAY_PRESENT_ROOM_RCV";
-  static TEST_SIZE = 10;
 
   emitter = null;
   receiver = null;
@@ -34,66 +38,76 @@ class BirthdayPresent {
     this.emitter = emitter;
     this.receiver = receiver;
 
-    receiver(BirthdayPresent.ROOM_SEND, (identifier) => {
-      emitter(BirthdayPresent.ROOM_RCV, identifier);
+    receiver(DeliveryService.ROOM_SEND, (identifier) => {
+      emitter(DeliveryService.ROOM_RCV, identifier);
     });
   }
 
-  // takes in an emitter and a receiver function. For SocketIO you might use calculateLatency like so:
-  // calculateLatency(socket.emit, socket.on)
-  calculateLatency() {
-    this.startTimes = {};
-    this.maxLatency = 0;
+  calculateLatency({ testSize = 10, persistent = false, interval = 1000 }) {
+    const work = () => {
+      this.startTimes = {};
+      this.maxLatency = 0;
 
-    receiver(BirthdayPresent.ROOM_RCV, (identifier) => {
-      const latency = Date.now() - this.startTimes[identifier];
+      receiver(DeliveryService.ROOM_RCV, (identifier) => {
+        const latency = Date.now() - this.startTimes[identifier];
 
-      if (latency > this.maxLatency) {
-        this.maxLatency = latency;
+        if (latency > this.maxLatency) {
+          this.maxLatency = latency;
+        }
+      });
+
+      for (let i = 0; i < testSize; i++) {
+        const identifier = Math.random();
+
+        this.startTimes[identifier] = Date.now();
+
+        emitter(DeliveryService.ROOM_SEND, identifier);
       }
-    });
+    };
 
-    for (let i = 0; i < BirthdayPresent.TEST_SIZE; i++) {
-      const identifier = Math.random();
-
-      this.startTimes[identifier] = Date.now();
-
-      emitter(BirthdayPresent.ROOM_SEND, identifier);
+    if (persistent) {
+      setInterval(work, interval);
+    } else {
+      work();
     }
   }
 
-  wrapPresent(message) {}
+  wrap(message) {
+    return new Present(message, new Date() + this.maxLatency);
+  }
 }
 
 class Present {
-  data;
+  message;
   openDate;
+  dateWrapped;
 
   constructor(data, openDate) {
     this.data = data;
     this.openDate = openDate;
+    this.dateWrapped = Date.now();
   }
 
   open(callback) {
     return new Promise((resolve) => {
       setTimeout(() => {
-        if (callback) callback(this.data);
-        resolve(this.data);
+        if (callback) callback(this.message);
+        resolve(this.message);
       }, this.openDate - Date.now());
     });
   }
+
+  setMessage(message) {
+    this.message = message;
+  }
+
+  setDate(date) {
+    this.openDate = date;
+    this.dateWrapped = Date.now();
+  }
+
+  rewrap() {
+    this.openDate = Date.now() + (this.openDate - this.dateWrapped);
+    this.dateWrapped = Date.now();
+  }
 }
-
-const emitter = (room, msg) => {};
-
-const receiver = (room, msg) => {};
-
-const p = new Present("woohooo", new Date(2023, 1, 24, 6, 37, 40, 0));
-
-const d = p.open((data) => {
-  console.log(data);
-});
-
-d.then((data) => {
-  console.log(data);
-});
